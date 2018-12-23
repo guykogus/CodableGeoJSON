72ad4cb3256ae2de1b13de40f8da5ed9a34904a7
Guy Kogus <guy.kogus@gmail.com>
2018-12-23 14:37:13 +1100

    Remove Pods folder

--- Pods/Target Support Files/Pods-SwiftCodableGeoJSON/Pods-SwiftCodableGeoJSON-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftCodableGeoJSON : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftCodableGeoJSON
-@end
--- Pods/Target Support Files/SwiftCodableJSON/SwiftCodableJSON-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftCodableJSON : NSObject
-@end
-@implementation PodsDummy_SwiftCodableJSON
-@end
